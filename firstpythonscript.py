7ff72660bb5f8983d1c3d347c05166a673327d6d
Angel Gonzalez Guevara <angelgonzalezguevara@angels-mbp-2.attlocal.net>
2024-01-01 20:35:43 -0800

    adding some details from Eulers branch

--- firstpythonscript.py
+++ firstpythonscript.py
@@ -2,11 +2,26 @@
 import numpy as np
 import sympy as sp
 
-x ,t = sp.symbols('x t')
+t, y = sp.symbols("y t")
 
-poly = sp.expand((x-2)*(t+3))
+UserODE = input("Enter ODE (in terms of t & y): y' =  ")
 
-print(poly.subs({x:2,t:1}))
+ODE = sp.sympify(UserODE)
+
+variables = (ODE.free_symbols)
+
+
+if t in variables and y in variables:
+    t0= int(input('write an initial point for t(to): '))
+    y0= int(input('write an initial point for y(yo): '))
+    InitODE = ODE.subs({t:t0,y:y0})
+
+
+
+
+# Display the identified ODE at certain pts.
+print("The variables in the function are:", InitODE)
+
 
 # Eulers Method w/ User's Choice of ODE
 
