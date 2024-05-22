33e79a5c9795a71d66c557cc91ba8cff024e4b15
AngelGonzalezGuevara <angelgonzalezguevara@angels-mbp-2.attlocal.net>
2024-05-22 12:09:28 -0700

    Added plotting features to look at numerical solution and comparing it with known tru solution. As well modified some print statements for neatness of the script

--- firstpythonscript.py
+++ firstpythonscript.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import sympy as sp
+import matplotlib.pyplot as mp
 
 t, y = sp.symbols("t y")
 
@@ -17,7 +18,7 @@ else:
 variables = (ODE.free_symbols)
 
 
-if t in variables and y in variables:
+if t in variables or y in variables:
     t0= float(f"{float(input('write the initial point, t0 = ')):.{4}f}")
     y0= float(f"{float(input('And now y(t0) = y0 = ')):.{4}f}")
     InitODE = ODE.subs({t:t0,y:y0})
@@ -29,12 +30,35 @@ yoft = float(f"{float(input('Up to what y(t) do you want an answer?(choose t for
 h = float(f"{float(input('what step size do you want?: ')):.{4}f}")
 
 t_ = np.arange(t0, yoft + h ,h)
-
+ysol = []
 for i in t_:
     yn = y0 + h * ODE.subs({t:i,y:y0})
+    ysol.append(yn)
     y0 = yn
 
-print(" at t = " + str(yoft) + ", y(" + str(yoft) + ")=" + str(yn)  )
+print(f"at t = {yoft} , y({yoft}) = {(round(yn, 3))}")
 
 # Eulers Method w/ User's Choice of ODE
 
+## Plugging in true soln. to 
+# see accuracy of the method
+
+print("Error Analysis: ")
+tru = input("Input true solution: ")
+
+if 'e' in tru:
+    true = tru.replace('e',str(sp.E))  # If D.E has an Eulers Number,
+    true = sp.sympify(true)
+else:
+    true = sp.sympify(tru)
+ypts =[]
+for i in t_:
+    ytrue = true.subs({t:i})
+    ypts.append((ytrue))
+
+mp.plot(t_,ysol,label= "Numerical Soln")
+mp.plot(t_,ypts,label= "True Soln")
+mp.xlabel("t")
+mp.ylabel("y")
+mp.legend()
+mp.show()
